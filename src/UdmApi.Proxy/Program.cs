fab7247d73059c0d63280ced681d00abd089d055
Mark Lopez <m@silvenga.com>
2020-02-02 13:02:37 -0600

    Added AspNetCore support.

--- src/UdmApi.Proxy/Program.cs
+++ src/UdmApi.Proxy/Program.cs
@@ -1,12 +1,17 @@
-﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
 
 namespace UdmApi.Proxy
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            CreateHostBuilder(args).Build().Run();
         }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder => { webBuilder.UseStartup<Startup>(); });
     }
 }
