11b2cda2c5cfac0c8b80ebe25b6bae8ce238efb4
Mark Lopez <m@silvenga.com>
2020-02-02 12:53:47 -0600

    Added project structure.

---
+++ src/UdmApi.Proxy/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace UdmApi.Proxy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
